4cda85d91b62603d2367d772b972daeabe3a4509
mtomeo85@live.com <>
2019-05-01 11:57:38 -0400

    This is my first commit

---
+++ src/javaapplication255/JavaApplication255.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication255;
+
+/**
+ *
+ * @author tomeo.mel
+ */
+public class JavaApplication255 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cottrell();
+    }
+    public static void Cottrell()
+    {
+        System.out.println("Hello World");
+    }
+}
